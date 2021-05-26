b96e8862bfa175d6612335f39caf4b8a376b5a9c
FlyingPig <zhongyinmin@pku.edu.cn>
2021-05-26 22:35:02 +0800

    prog4 finished

--- prog4_sqrt/main.cpp
+++ prog4_sqrt/main.cpp
@@ -34,7 +34,12 @@ int main() {
         // to you generate best and worse-case speedups
         
         // starter code populates array with random input values
-        values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // random : values[i] = .001f + 2.998f * static_cast<float>(rand()) / RAND_MAX;
+        // best case : values[i] = 2.998f;
+        // worst case for SIMD
+        if (i % 8 == 0) values[i] = 2.998f;
+        else values[i] = 1.f;
+        
     }
 
     // generate a gold version to check results
